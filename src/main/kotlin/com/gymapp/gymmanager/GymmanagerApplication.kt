a50ceedc87fc4d9afc784a74580535df806ebc2b
chunchiter <rafacastajr5@gmail.com>
2026-04-17 18:34:33 -0600

    first commit

---
+++ src/main/kotlin/com/gymapp/gymmanager/GymmanagerApplication.kt
@@ -0,0 +1,11 @@
+package com.gymapp.gymmanager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GymmanagerApplication
+
+fun main(args: Array<String>) {
+	runApplication<GymmanagerApplication>(*args)
+}
